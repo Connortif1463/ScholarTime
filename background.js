4add8366e69bfddc047132f654607ac584d5e538
Connortif1463 <66648411+Connortif1463@users.noreply.github.com>
2024-04-28 22:42:09 -0400

    removing functionality

--- background.js
+++ background.js
@@ -1,26 +1,34 @@
 // background.js
-var storedUrl = null;
+// var storedUrl = null;
+
+// // Listener for forbidden website link
+// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+//     if (message.action == "storeUrl") {
+//         storedUrl = message.url;
+//     }
+// });
+
+// // When requested, give stored URL to popup.js
+// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+//     if (message.action == "getUrl") {
+//         sendResponse({url: storedUrl});
+//     }
+// });
+
+// // Chrome action listener
+// chrome.action.onClicked.addListener((tab) => {
+//     chrome.windows.create({
+//         url: "toggle.html",
+//         type: "popup",
+//         width: 200,
+//         height: 100
+//     });
+// });
 
 // Listener for forbidden website link
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.action == "storeUrl") {
-        storedUrl = message.url;
+        // Open a new tab with the forbidden page
+        chrome.tabs.update(sender.tab.id, { url: chrome.runtime.getURL("forbidden.html") });
     }
-});
-
-// When requested, give stored URL to popup.js
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.action == "getUrl") {
-        sendResponse({url: storedUrl});
-    }
-});
-
-// Chrome action listener
-chrome.action.onClicked.addListener((tab) => {
-    chrome.windows.create({
-        url: "toggle.html",
-        type: "popup",
-        width: 200,
-        height: 100
-    });
 });
