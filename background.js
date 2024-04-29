10dc9872139da8b481ae86f42b27fa7f5bf40fd8
Connortif1463 <66648411+Connortif1463@users.noreply.github.com>
2024-04-28 22:44:53 -0400

    background and forbidden

--- background.js
+++ background.js
@@ -1,19 +1,4 @@
 // background.js
-// var storedUrl = null;
-
-// // Listener for forbidden website link
-// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-//     if (message.action == "storeUrl") {
-//         storedUrl = message.url;
-//     }
-// });
-
-// // When requested, give stored URL to popup.js
-// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-//     if (message.action == "getUrl") {
-//         sendResponse({url: storedUrl});
-//     }
-// });
 
 // // Chrome action listener
 // chrome.action.onClicked.addListener((tab) => {
